e5140c0a44ca09596b4467c68106624f26adca0b
wishfully28 <thewishfulthinker28@gmail.com>
2024-01-09 16:49:11 +0200

    Commit and push
    KotlinBasics and VariableTypes

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+
+    var myAge : Byte = 21
+    println(myAge)
+    myAge = 22
+    println(myAge)
+}
---
+++ app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbasics
+
+fun main(){
+    val constantNumber = 1
+    var mutableString = "a string lol"
+    println(constantNumber)
+    println(mutableString)
+    mutableString = "still a string lol"
+    println(mutableString)
+
+}
