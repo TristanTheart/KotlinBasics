1495ba70c486e40370e52883afce1f5633ac00c6
wishfully28 <thewishfulthinker28@gmail.com>
2024-01-10 17:57:23 +0200

    Day 2 - Booleans, If-else statements and other  logical operators

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,9 +2,26 @@ package com.example.kotlinbasics
 
 fun main(){
 
+    var age = 21
 
-    var myAge : Byte = 21
-    println(myAge)
-    myAge = 22
-    println(myAge)
+    // Comparison operators
+    // greater than >
+    // less than <
+    // greater equal >=
+    // less equal <=
+    // equal ==
+    println("Please enter your age as a whole number")
+    // Read what the user has entered into the console
+     val enteredValue = readln()
+    // convert the str var into int var
+     age = enteredValue.toInt()
+        if(age in 18..39){
+        println("You can enter")
+            }else if(age >= 40){
+            println("You are too old!")
+                 }else{
+                println("You are too young to go into the club!")
+
+
+    }
 }
