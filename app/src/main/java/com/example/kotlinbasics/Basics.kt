cc1b2839bef10d1e76f88e99395a419d334c5e3c
wishfully28 <thewishfulthinker28@gmail.com>
2024-01-16 22:56:49 +0200

    Added list.

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,2 +1,22 @@
 package com.example.kotlinbasics
 
+fun main(){
+    //Immutable list - you cannot add items after init
+    //val shoppingList = listOf("CPU", "RAM" , "GPU" , "SSD")
+    //Mutable List - you can add/modify items
+    val shoppingList = mutableListOf("CPU", "RAM" , "GPU RTX 3060" , "SSD")
+    shoppingList.add("HSF")
+    shoppingList.remove("GPU RTX 3060")
+    shoppingList.add("GPU RTX 4060")
+
+    println(shoppingList)
+    shoppingList.add("RAM")
+    println(shoppingList[3])
+shoppingList.remove("GPU RTX 4060")
+    shoppingList[3] = "RX 7800XT"
+    println(shoppingList)
+    shoppingList.set(1,"Water Cooling")
+    println(shoppingList)
+
+
+}
